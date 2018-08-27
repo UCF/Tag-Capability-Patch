f67f9d8ea97d6321fc41633690479237d3d66817
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-08-27 11:43:45 -0400

    Added foreach loop to account for multiple tags being added

--- src/js/tag-metabox-fix.js
+++ src/js/tag-metabox-fix.js
@@ -3,24 +3,52 @@
     $('.newtag').on('keypress', (event) => {
       if (event.keyCode === 13 && event.tagChecked !== false) {
 
-        const $input = $(event.currentTarget).val();
+        const inputArray = $(event.currentTarget).val().split(',');
+        const $tagInput = $('#tax-input-post_tag');
+        const disallowedTags = [];
+        let inputChecks = 0;
 
-        const data = {
-          action: 'ajax-tag-search',
-          tax: 'post_tag',
-          q: $input
-        };
+        inputArray.forEach((input) => {
+          const data = {
+            action: 'ajax-tag-search',
+            tax: 'post_tag',
+            q: input.trim()
+          };
 
-        $.get(ajaxurl, data, (response) => {
-          if (response === '') {
+          $.get(ajaxurl, data, (response) => {
+            if (response === '') {
+              disallowedTags.push(input);
+
+              // Remove tag list item
+              $(`li:contains('${input}')`).remove();
+
+              // Get current value of tag input and remove the tag.
+              const $tags = $tagInput.val().split(',');
+              $tags.splice($.inArray(input, $tags), 1);
+              $tagInput.val($tags.join(','));
+
+              // Increment our inputChecks variable
+              inputChecks++;
+            }
+          });
+        });
+
+        const onExecutionFinish = () => {
+          if (inputArray.length > inputChecks) {
+            return;
+          }
+
+          if (disallowedTags.length > 0) {
             $('.newtag').pointer({
-              content: `<p>The tag "${$input}" has been removed from the tag list. You currently have permission to add existing tags.</p>`,
+              content: `<p>The following tags have been removed: <span style="color: red">${disallowedTags.join(', ')}</span>. You do not have permission to publish new tags.</p>`,
               position: 'bottom'
             }).pointer('open');
-
-            $(`li:contains('${$input}')`).remove();
           }
-        });
+
+          window.clearInterval(checkInterval);
+        };
+
+        const checkInterval = setInterval(onExecutionFinish, 25);
       }
     });
   };
